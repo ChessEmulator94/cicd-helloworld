3afffcee13a9310ec0438de3d5fecddb4d35b05f
Gadi Friedman <guppiefriedman@gmail.com>
2025-11-14 22:06:16 +0100

    Initial commit

---
+++ src/main/java/com/chessemulator94/cicd_helloworld/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.chessemulator94.cicd_helloworld.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/generic_greeting")
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/greet_me")
+    public String getHelloPerson(@RequestParam String name) {
+        return "Hello World Master " + name;
+    }
+}
---
+++ src/test/java/com/chessemulator94/cicd_helloworld/CicdHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessemulator94.cicd_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
